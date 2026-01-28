a892e26ccbd2e6801c2728aa4a043b14b909250e
Yaswanth <potnuruyaswanthcr7@gmail.com>
2026-01-28 23:46:30 +0530

    upto password hashing

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 })
+
+app.post('/register',async(req,res)=>{
+    const {username,password}=req.body;
+    console.log(username,' & ',password);
+    const hashedpassword=await bcrypt.hash(password,10);
+    res.send({
+        entered_usernane:username,
+        entered_password:password,
+        hashedpassword:hashedpassword,
+    })
+})
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
